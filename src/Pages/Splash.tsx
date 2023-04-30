7f5e4e9ad05a9731cbc9d2bd895bbfce7369134d
Callum Buttery <callumbuttery@icloud.com>
2023-04-30 16:31:46 +0100

    Fix: syntax error

--- src/Pages/Splash.tsx
+++ src/Pages/Splash.tsx
@@ -1,7 +1,7 @@
 export const Splash: React.FC = () => {
     return (
         <div className="h-screen bg-[url('assets/splash.jpg')] bg-no-repeat bg-cover bg-center bg-fixed flex justify-center items-center">
-            <div className="text-7xl text-whitew font-serif backdrop-blur-sm px-64 py-64 tracking-widest">
+            <div className="text-7xl text-white font-serif backdrop-blur-sm px-64 py-64 tracking-widest">
                 COMING SOON
             </div>
         </div>
