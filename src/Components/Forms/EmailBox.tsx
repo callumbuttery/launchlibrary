eba32f3c3778a17e92119d0fbb709c79a5ba668e
Callum Buttery <callumbuttery@icloud.com>
2023-05-21 17:19:11 +0100

    Fix: update styling of splash

--- src/Components/Forms/EmailBox.tsx
+++ src/Components/Forms/EmailBox.tsx
@@ -19,9 +19,9 @@ export const EmailBox: React.FC = () => {
                 {valid && success && <p className="text-green-600 text-sm mr-8 mb-2">
                     Email received!
                 </p>}
-                <div className="flex justify-center h-24">
-                    <input onChange={(e) => checkEmail(e)} className="border bg-black w-full text-white px-2 ml-24 h-10 w-96 leading-tight focus:outline-none mr-4" type="email" placeholder="Email" aria-label="Full name" />
-                    <button onClick={(e) => submitEmail(e)} className="h-10 flex-shrink-0 bg-sky-500 hover:bg-teal-700 border-sky-500 hover:border-sky-400 hover:bg-sky-400 text-sm border-1 text-white py-1 px-2 rounded" type="button">
+                <div className="h-24">
+                    <input onChange={(e) => checkEmail(e)} className="flex justify-center border bg-black w-full text-white px-2 ml-24 h-10 w-64 leading-tight focus:outline-none mr-4" type="email" placeholder="Email" aria-label="Full name" />
+                    <button onClick={(e) => submitEmail(e)} className="h-10 ml-4 mt-2 flex-shrink-0 bg-sky-500 hover:bg-teal-700 border-sky-500 hover:border-sky-400 hover:bg-sky-400 text-sm border-1 text-white py-1 px-2 rounded" type="button">
                         Get updates!
                     </button>
                 </div>
--- src/Pages/Splash.tsx
+++ src/Pages/Splash.tsx
@@ -4,7 +4,8 @@ export const Splash: React.FC = () => {
     return (
         <div className="h-screen bg-[url('assets/annie-spratt-F2M5Uynt62U-unsplash.jpg')] bg-no-repeat bg-cover bg-center bg-fixed flex justify-center items-center">
             <div>
-                <span className="text-7xl text-gray-500 font-serif tracking-widest">COMING SOON</span>
+                <span className="flex justify-center text-7xl text-gray-500 font-serif tracking-widest">Ark Insight</span>
+                <span className="mt-5 flex justify-center text-3xl text-gray-400 font-serif tracking-widest">COMING SOON</span>
                 <EmailBox/>
             </div>
         </div>
