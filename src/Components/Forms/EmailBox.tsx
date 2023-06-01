26eec8d34decb481855e90ccd9c4db1c17c2b68c
Callum Buttery <callumbuttery@icloud.com>
2023-06-01 17:52:01 +0100

    Fix: styling fixes

--- src/Components/Forms/EmailBox.tsx
+++ src/Components/Forms/EmailBox.tsx
@@ -12,16 +12,18 @@ export const EmailBox: React.FC = () => {
 
     return (
         <div className='justify-center text-center'>
-            <form className="max-w-md mt-10">
+            <form className="mt-10">
                 {!valid && email.length > 0 && <p className="text-blue-300 text-sm mr-8 mb-2">
                     Please provide a valid email address.
                 </p>}
                 {valid && success && <p className="text-green-600 text-sm mr-8 mb-2">
                     Email received!
                 </p>}
-                <div className="h-24 ml-24">
-                    <input onChange={(e) => checkEmail(e)} className="flex justify-center border bg-transparent w-full text-white px-2 ml-0 h-10 w-64 leading-tight focus:outline-none mr-4" type="email" placeholder="Email" aria-label="Full name" />
-                    <button onClick={(e) => submitEmail(e)} className="h-10 ml-4 mt-2 flex-shrink-0 bg-orange-500 hover:bg-orange-300 text-sm border-1 text-white py-1 px-2 rounded" type="button">
+                <div className="h-10 w-64 grid grid-cols-1 mx-auto">
+                    <div className=''>
+                        <input onChange={(e) => checkEmail(e)} className="flex justify-center border bg-transparent w-full text-white px-2 ml-0 h-10 w-64 leading-tight focus:outline-none mr-4" type="email" placeholder="Email" aria-label="Full name" />
+                    </div>
+                    <button onClick={(e) => submitEmail(e)} className="h-10 w-24 mx-auto mt-2 flex-shrink-0 bg-orange-500 hover:bg-orange-300 text-sm border-1 text-white py-1 px-2 rounded" type="button">
                         Get updates!
                     </button>
                 </div>
